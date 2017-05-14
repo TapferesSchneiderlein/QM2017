e68157e58d50a1185d59368b7daebc6134c3ba75
TapferesSchneiderlein <stefan.schneider.email@web.de>
2017-05-14 23:43:21 +0200

    getPercentage() implemented Test Config added

--- de.uhd.ifi.se.quizapp/src/de/uhd/ifi/se/quizapp/model/twochoiceexercise/TwoChoiceResult.java
+++ de.uhd.ifi.se.quizapp/src/de/uhd/ifi/se/quizapp/model/twochoiceexercise/TwoChoiceResult.java
@@ -23,5 +23,44 @@ public class TwoChoiceResult extends Result {
 	public String toString() {
 		return this.booleanStatements.toString();
 	}
+	
+	/**
+	* The method calculates and returns the percentage of correct
+	* answers for a TwoChoiceExercise.
+	*
+	* Input of the method:
+	*	1. 	TwoChoiceExercise containing statements which can be
+	*		true or false. (accessed through getExercise())
+	*	2. 	TwoChoiceResult containing the solution for the
+	*		exercise statements. (accessed through this)
+	*
+	* Output of the method:
+	*	The percentage of correct answers as decimal.
+	*	The percentage cannot be less than zero or greater than
+	*	one (0 <= percentage <= 1).
+	*	Returns zero if an exercise contains no statements.
+	*	Returns -1 on invalid inputs.
+	*/
+	public float getPercentage() {		
+		if ( this.booleanStatements == null ) {
+			return -1;
+		}
+		
+		TwoChoiceExercise ex = (TwoChoiceExercise)getExercise();
+		ArrayList<BooleanStatement> exStatements = ex.getBooleanStatements();
+		
+		if(exStatements.isEmpty()){
+			return 0;
+		}
+		float correct = 0;
+		for(int i = 0; i<exStatements.size(); i++){
+			boolean answer = exStatements.get(i).isCorrect();
+			boolean solution =  this.booleanStatements.get(i).isCorrect();
+			if (answer == solution){
+				correct++;
+			}
+		}
+		return correct / (float)exStatements.size();		
+	}
 
 }
---
+++ de.uhd.ifi.se.quizapp/src/de/uhd/ifi/se/quizapp/test/TestGetPercentageInTwoChoiceResult.java
@@ -0,0 +1,188 @@
+package de.uhd.ifi.se.quizapp.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.uhd.ifi.se.quizapp.model.twochoiceexercise.BooleanStatement;
+import de.uhd.ifi.se.quizapp.model.twochoiceexercise.TwoChoiceExercise;
+import de.uhd.ifi.se.quizapp.model.twochoiceexercise.TwoChoiceResult;
+
+public class TestGetPercentageInTwoChoiceResult {
+
+	@Test
+	public void testExerciseOneWrongStatementWrongSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", true));
+
+		assertTrue(result.getPercentage() == 0);
+	}
+
+	@Test
+	public void testExerciseOneWrongStatementCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+
+		assertTrue(result.getPercentage() == 1);
+	}
+
+	@Test
+	public void testExerciseOneCorrectStatementWrongSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+
+		assertTrue(result.getPercentage() == 0);
+	}
+
+	@Test
+	public void testExerciseOneCorrectStatementCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", true));
+
+		assertTrue(result.getPercentage() == 1);
+	}
+
+	@Test
+	public void testExerciseTwoWrongStatementsWrongSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", false));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", true));
+		result.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		assertTrue(result.getPercentage() == 0);
+	}
+
+	@Test
+	public void testExerciseTwoWrongStatementsPartlyCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", false));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+		result.getBooleanStatements().add(new BooleanStatement("B", true));
+		
+		float perc = result.getPercentage();
+		 assertTrue(result.getPercentage() == 0.5);
+	}
+
+	@Test
+	public void testExerciseTwoWrongStatementsCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", false));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+		result.getBooleanStatements().add(new BooleanStatement("B", false));
+
+		assertTrue(result.getPercentage() == 1);
+	}
+
+	@Test
+	public void testExerciseTwoPartlyCorrectStatementsWrongSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", true));
+		result.getBooleanStatements().add(new BooleanStatement("B", false));
+
+		assertTrue(result.getPercentage() == 0);
+	}
+
+	@Test
+	public void testExerciseTwoPartlyCorrectStatementsPartlyCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+		result.getBooleanStatements().add(new BooleanStatement("B", false));
+
+		assertTrue(result.getPercentage() == 0.5);
+	}
+
+	@Test
+	public void testExerciseTwoPartlyCorrectStatementsCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", false));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+		result.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		assertTrue(result.getPercentage() == 1);
+	}
+
+	@Test
+	public void testExerciseTwoCorrectStatementsWrongSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", true));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+		result.getBooleanStatements().add(new BooleanStatement("B", false));
+
+		assertTrue(result.getPercentage() == 0);
+	}
+
+	@Test
+	public void testExerciseTwoCorrectStatementsPartlyCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", true));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", false));
+		result.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		assertTrue(result.getPercentage() == 0.5);
+	}
+
+	@Test
+	public void testExerciseTwoCorrectStatementsCorrectSolution() {
+		TwoChoiceResult result = new TwoChoiceResult();
+		TwoChoiceExercise exercise = new TwoChoiceExercise();
+		exercise.getBooleanStatements().add(new BooleanStatement("A", true));
+		exercise.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		result.setExercise(exercise);
+		result.getBooleanStatements().add(new BooleanStatement("A", true));
+		result.getBooleanStatements().add(new BooleanStatement("B", true));
+
+		assertTrue(result.getPercentage() == 1);
+	}
+
+}
